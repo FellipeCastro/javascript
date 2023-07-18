c733d8c95915baf1c9d84427dd0a4982933df114
FellipeCastro <fellipecastro27d@gmail.com>
2023-07-18 18:23:10 -0300

    exercícios da aula 12 criados

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 49
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade >= 16 && idade < 18 || idade > 65 ) {
+        console.log('Voto opicional')
+} else {
+    console.log('Voto obrigatório')
+}
+    
