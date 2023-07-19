b820976830f4a52e18adf4ac33dc92b71cb196c5
FellipeCastro <fellipecastro27d@gmail.com>
2023-07-19 18:10:50 -0300

    exercícios das aulas 13 e 14 feitos

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 10)
+
+
+/*
+
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+...
+*/
