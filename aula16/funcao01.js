c1897bd926f72552c06c02d481287fbac11a839c
FellipeCastro <fellipecastro27d@gmail.com>
2023-07-21 18:38:06 -0300

    exercício final finalizado

---
+++ aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+//Jogando o valor em uma var
+let res = parimpar(4)
+console.log(res)
+
+//Mostrando o valor direto
+console.log(parimpar(34))
